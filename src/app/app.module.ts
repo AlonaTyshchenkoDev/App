5d20cc2ccdce5a828d24c3e16f7ad9f62153eeb5
Alona_Tyshchenkogit config --global user.email alona.tyshchenko.dev.gmail.com <alona.tyshchenko.dev@gmail.com>
2022-07-19 08:28:44 +0300

    creating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { CustomSelectComponent } from './custom-select/custom-select.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    LoginPageComponent,
+    CustomSelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
